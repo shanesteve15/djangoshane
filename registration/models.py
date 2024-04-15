84f1c3c910d192697ebd7e84133f8999d2e12487
shanesteve15 <shanekanonga430@gmail.com>
2024-04-15 02:46:48 -0400

    django2

--- registration/models.py
+++ registration/models.py
@@ -3,7 +3,8 @@ from django.db import models
 class student(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    date_of_birth = models.DateField()
     email = models.EmailField(unique=True)
-    age = models.IntegerField(default=0)
+    country = models.CharField(max_length=100)
+    password = models.CharField(max_length=90)
+
 # Create your models here.
